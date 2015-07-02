07f3f52ff43e1f3865d8aae0f112be268af45ccc
John Mai <maicorp@gmail.com>
2015-07-02 01:37:47 -0700

    (cleanup) move the colors into colorpickerview

--- client/js/models/PictureModel.js
+++ client/js/models/PictureModel.js
@@ -6,7 +6,9 @@ app.PictureModel = Backbone.Model.extend({
   //defaults: {
     //container: d3.select('body')
   //},
-  currentColor: '#000000',
+  selectedColor: '#000000',
+  selectedWidth: '4',
+  selectedFill: 'none',
 
   initialize: function() {
     this.set('lines', new app.LineCollection());
@@ -17,13 +19,22 @@ app.PictureModel = Backbone.Model.extend({
     //this.set('activeLine', new app.LineModel({id: idHash()}));
     this.set('activeLine', new app.LineModel());
     // console.log(this.)
-    this.get('activeLine').set('stroke', this.get('currentColor'));
+    this.get('activeLine').set('stroke', this.get('selectedColor'));
+    this.get('activeLine').set('fill', this.get('selectedFill'));
+    this.get('activeLine').set('stroke_width', this.get('selectedWidth'));
     this.get('lines').add(this.get('activeLine'));
   },
 
   changeColor: function(color) {
-    console.log('called changecolor to ', color);
-    this.set('currentColor', color);
+    this.set('selectedColor', color);
+  },
+
+  changeWidth: function(width) {
+    this.set('selectedWidth', width);
+  },
+
+  changeFill: function(fill) {
+    this.set('selectedFill', fill);
   },
 
   drag: function(mouseCoord) {
