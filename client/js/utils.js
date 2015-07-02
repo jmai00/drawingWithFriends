a76652cc1b06b452979cbfdcb085832a844e6c44
John Mai <maicorp@gmail.com>
2015-07-02 16:01:39 -0700

    (fix) added a new GUID generator following standardv4

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,10 +1,21 @@
 //use this guid generator as the id hashing fn to generate a unique id
 function guid() {
   function s4() {
+
     return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
   }
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
     s4() + '-' + s4() + s4() + s4();
+}
+
+function generateUUID(){
+    var d = performance.now();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
