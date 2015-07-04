9c9cf6d3071c9afa2f86286fc12d5b0a12205222
John Mai <maicorp@gmail.com>
2015-07-03 17:53:03 -0700

    (fix) fix user chat to work

--- client/js/views/ChatView.js
+++ client/js/views/ChatView.js
@@ -5,29 +5,51 @@ app.ChatView = Backbone.View.extend({
   className: 'chat',
   initialize: function() {
     this.render();
-    socket.on('user message', function(data){});
+    socket.on('user message', function(data){
+      console.log("Got Message");
+      this.addMessage(data);
+    }.bind(this));
+
   },
 
-  template: _.template('<span class="username"/><%- user -></span><span class="message"><%- message -></span>'),
+  template: _.template('<li><span class="username"/><%- user -></span><span class="message"><%- message -></span></li>'),
   
   events: {
-    'onclick': function(event) {
+    'click .clearButton': function(event) {
+      this.clearMessage();
+    },
+
+    'keypress .inputMessage': function(event) {
+      if (event.keyCode === 13) {
+        this.sendMessage();
+      }
+
     }
   },
 
   render: function() { 
-    this.$el.append('<div class="chatArea"> \
-        <ul class="messages"></ul> \
-      </div> \
-      <input class="inputMessage" placeholder="Type here..."/>');
-      
+    this.$el.append(
+      '<input class="inputMessage" placeholder="Type here..."/> \
+      <input type="button" value="Clear" class="clearButton"> \
+      <div class="chatArea"><ul class="messages"></ul></div>');
+  },
+
+  sendMessage: function() {
+    var message = _.escape($('.inputMessage').val());
+    $('.inputMessage').val('');
+
+    var data = { username: app.username, message: message };
+    socket.emit('user message', data);
+    this.addMessage(data);
+    
   },
 
   addMessage: function(data) {
-    this.$el.append(this.template(data));
+    $('.messages').append(
+      '<li><span class="username">' + data.username + '</span>: <span class="message">' + data.message + '</span></li>');
   },
 
   clearMessage: function() {
-    this.$el.empty();
+    $('.messages').empty();
   }
 });
